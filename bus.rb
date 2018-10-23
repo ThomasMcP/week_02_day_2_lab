25a62d21c23622ee6040b6b381c3046d40fdaaad
Alex Clements <alexfjclements@gmail.com>
2018-10-23 15:32:52 +0100

    end of lab backup.

--- bus.rb
+++ bus.rb
@@ -1,14 +1,25 @@
 class Bus
 
-  attr_reader :route_number, :destination
+  attr_reader :route_number, :destination, :passengers
 
   def initialize(route_number, destination)
     @route_number = route_number
     @destination = destination
+    @passengers = []
   end
 
   def drive()
     return "Brum brum"
+  end
+
+  def pick_up(passenger)
+    @passengers << passenger
+  end
+
+  def drop_off(passenger)
+    for person in @passengers
+      @passenger.delete(passenger) if person == passenger
+    end
   end
 
 end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -1,10 +1,14 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../bus")
+require_relative('../person')
 
 class BusTest < MiniTest::Test
 
   def setup
+    @tom = Person.new("Tom", 27)
+    @alex = Person.new("Alex", 29)
+    @pawel = Person.new("Pawel", 31)
     @bus1 = Bus.new(22, "Ocean Terminal")
   end
 
@@ -24,6 +28,24 @@ class BusTest < MiniTest::Test
     expected = "Brum brum"
     actual = @bus1.drive
     assert_equal(expected, actual)
+  end
+
+  def test_number_of_passengers__0
+    assert_equal(0, @bus1.passengers.length)
+  end
+
+  def test_number_of_passengers__added_1
+    expected = 1
+    @bus1.pick_up(@tom)
+    actual = @bus1.passengers.length
+    assert_equal(expected, actual)
+  end
+
+  def test_passenger_off_bus
+    # @bus1.pick_up(@tom)
+    expected = 0
+    # @bus1.drop_off(@tom)
+    assert_equal(expected, @bus1.passengers.length)
   end
 
 end
